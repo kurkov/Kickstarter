14e4756dd2ad9f7aaa26df2d373dd5b5b4b367e5
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2015-05-04 22:04:32 +0300

    added createBlogPostController method to Factory

--- src/main/java/ua/goit/kickstarter/factory/Factory.java
+++ src/main/java/ua/goit/kickstarter/factory/Factory.java
@@ -53,6 +53,20 @@ public class Factory {
     return controller;
   }
 
+  public static Controller createBlogPostController(Class<? extends
+          Controller> clazz, Connection connection) {
+    Controller controller = null;
+    try {
+      Constructor<? extends Controller> constructor =
+              clazz.getConstructor(BlogPostService.class);
+      BlogPostService service = getBlogPostService(getBlogPostDao(connection));
+      controller = constructor.newInstance(service);
+    } catch (Throwable t) {
+      throw new RuntimeException(t);
+    }
+    return controller;
+  }
+
   protected static CategoryDao getCategoryDao(Connection connection) {
     return new CategoryDaoImpl(getConnection());
   }
