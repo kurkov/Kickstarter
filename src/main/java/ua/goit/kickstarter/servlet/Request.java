a48cfbea796da5dbb3c9d2c3be593e1e6a360c23
OlgaKulikova <791030@mail.ru>
2015-05-07 01:58:20 +0300

    equals hashcode

--- src/main/java/ua/goit/kickstarter/servlet/Request.java
+++ src/main/java/ua/goit/kickstarter/servlet/Request.java
@@ -8,7 +8,7 @@ public class Request {
   private final String method;
   private final String url;
 
-  public Request(Map<String, String[]> parameters, String method, String url) {
+  public Request(Map<String, String> parameters, String method, String url) {
     this.method = method.toUpperCase();
     this.url = url;
 
@@ -42,5 +42,33 @@ public class Request {
 
   public static Request create(String method, String url) {
     return new Request(null, method, url);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Request request = (Request) o;
+
+    if (method != null ? !method.equals(request.method) : request.method != null)
+      return false;
+    return !(url != null ? !url.equals(request.url) : request.url != null);
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = method != null ? method.hashCode() : 0;
+    result = 31 * result + (url != null ? url.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "Request{" +
+        "method='" + method + '\'' +
+        ", url='" + url + '\'' +
+        '}';
   }
 }
