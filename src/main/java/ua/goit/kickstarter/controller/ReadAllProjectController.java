f6f23f0c1414d65f4a33b405a1ec19b1154b86cd
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2015-05-11 23:43:38 +0300

    added ReadAllProjectController

---
+++ src/main/java/ua/goit/kickstarter/controller/ReadAllProjectController.java
@@ -0,0 +1,33 @@
+package ua.goit.kickstarter.controller;
+
+import ua.goit.kickstarter.model.Project;
+import ua.goit.kickstarter.service.ProjectService;
+import ua.goit.kickstarter.servlet.Request;
+import ua.goit.kickstarter.view.ViewModel;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.List;
+
+public class ReadAllProjectController implements Controller {
+
+  private final ProjectService projectService;
+
+  public ReadAllProjectController(ProjectService projectService) {
+    this.projectService = projectService;
+  }
+
+  @Override
+  public ViewModel process(Request request) throws ServletException, IOException {
+    return proceedGet(request);
+  }
+
+  private ViewModel proceedGet(Request request) {
+
+    ViewModel viewModel = new ViewModel("/WEB-INF/jsp/projects.jsp");
+    List<Project> projects = projectService.getAll();
+    viewModel.addAttributes("projects", projects);
+
+    return viewModel;
+  }
+}
