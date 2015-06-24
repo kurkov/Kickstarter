a6e6b465f5c6b2bfb3bc72bf3660b19eb03a450e
OlgaKulikova <791030@mail.ru>
2015-06-24 14:20:33 +0300

    hardcode user

--- src/main/java/ua/goit/kickstarter/dao/UserDaoImpl.java
+++ src/main/java/ua/goit/kickstarter/dao/UserDaoImpl.java
@@ -3,9 +3,13 @@ package ua.goit.kickstarter.dao;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
 import org.springframework.stereotype.Repository;
+import ua.goit.kickstarter.model.Roles;
 import ua.goit.kickstarter.model.User;
+import ua.goit.kickstarter.model.UserRole;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public class UserDaoImpl extends AbstractDao<User> implements UserDao {
@@ -26,6 +30,10 @@ public class UserDaoImpl extends AbstractDao<User> implements UserDao {
   public User getByName(String name) {
 //    return (User) sessionFactory.getCurrentSession().get(User.class, name);
 
-    return new User("olenenok", "olenenok", "olenenok", true, null);
+    Set<UserRole> roles = new HashSet<>();
+    User user = new User("olenenok", "olenenok", "masyakot@gmail.com", true);
+
+    roles.add(new UserRole(Roles.ADMIN, user));
+    return user;
   }
 }
