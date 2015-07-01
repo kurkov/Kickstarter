797ca32697f546a4543d853a0911f4f801bd81e4
OlgaKulikova <791030@mail.ru>
2015-07-01 21:22:02 +0300

    add sql requests

--- src/main/resources/sql/createTables.sql
+++ src/main/resources/sql/createTables.sql
@@ -9,3 +9,24 @@ CREATE TABLE projects (
   description TEXT,
   id_category INTEGER
 );
+
+CREATE TABLE users (
+  id INT KEY AUTO_INCREMENT,
+  username VARCHAR(50),
+  password VARCHAR(50),
+  email VARCHAR(50),
+  enable BOOLEAN
+);
+
+CREATE TABLE authorities (
+  id INT KEY AUTO_INCREMENT,
+  username VARCHAR(50),
+  authority VARCHAR(50)
+);
+
+CREATE TABLE user_authority(
+  user_id INTEGER(6) NOT NULL,
+  authority_id INTEGER(6) NOT NULL,
+  KEY user(user_id),
+  KEY authority(authority_id)
+);
